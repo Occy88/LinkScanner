79bc3fb84d5f68ff5eea993c51bbdb06c5862c52
emchoko <emilfrogy@gmail.com>
2017-12-02 12:45:18 +0000

    Camera working and returning where the file was the saved

--- app/src/main/java/com/linkscanner/linkscanner/MainActivity.java
+++ app/src/main/java/com/linkscanner/linkscanner/MainActivity.java
@@ -1,19 +1,34 @@
 package com.linkscanner.linkscanner;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v13.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.afollestad.materialcamera.MaterialCamera;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+
+    public static final int CAMERA_RQ = 4011;
+    private static final int MY_PERMISSIONS_CAMERA = 1234;
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @BindView(R.id.open_camera_button)
     Button button;
@@ -35,6 +50,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         .setAction("Action", null).show();
             }
         });
+
+
+        button.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        switch (requestCode) {
+            case MY_PERMISSIONS_CAMERA: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    openCamera();
+                } else {
+                    Toast.makeText(this, "You need to allow these permissions!", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
     }
 
     @Override
@@ -64,8 +100,72 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (view.getId()){
             case R.id.open_camera_button:
                 //Open camera here
-
+                if(checkCameraPermission()) {
+                    openCamera();
+                } else {
+                    requestCameraPermission();
+                }
                 break;
         }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        Log.d(TAG, "onActivityResult() " + requestCode + ";" + MY_PERMISSIONS_CAMERA);
+        switch (requestCode){
+            case CAMERA_RQ:
+                Log.d(TAG, "case" + requestCode);
+
+                if(resultCode == RESULT_OK){
+                    Toast.makeText(MainActivity.this, "Saved to:" + data.getDataString(), Toast.LENGTH_SHORT).show();
+                } else {
+                    Exception e = (Exception) data.getSerializableExtra(MaterialCamera.ERROR_EXTRA);
+                    e.printStackTrace();
+                    Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
+    private void requestCameraPermission() {
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.CAMERA)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.CAMERA},
+                        MY_PERMISSIONS_CAMERA);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+    }
+
+
+    private void openCamera(){
+        new MaterialCamera(this)
+                .stillShot()
+                .start(CAMERA_RQ);
+    }
+
+    private boolean checkCameraPermission() {
+        return ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
     }
 }
